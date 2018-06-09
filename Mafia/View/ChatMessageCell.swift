c80dbbd6b22e29951771c85848613c74bbc304bc
Eugene Lu <eugenelu9848@gmail.com>
2018-06-09 16:38:52 -0600

    Add name of sender to message cell

--- Mafia/View/ChatMessageCell.swift
+++ Mafia/View/ChatMessageCell.swift
@@ -19,27 +19,41 @@ class ChatMessageCell: UITableViewCell {
         let textView = UITextView()
         textView.translatesAutoresizingMaskIntoConstraints = false
         textView.text = "Sample"
+        textView.font = UIFont(name: "Helvetica", size: 16)
         textView.textColor = .white
         textView.isScrollEnabled = false
         textView.isUserInteractionEnabled = false
-        textView.backgroundColor = .clear
-        textView.layer.cornerRadius = 8
-        textView.layer.borderColor = UIColor.purple.cgColor
+        textView.backgroundColor = UIColor(rgb: 0x551A8B)
+        textView.layer.cornerRadius = 10
+        textView.layer.borderColor = UIColor(rgb: 0x551A8B).cgColor
         textView.layer.borderWidth = 2
-
         return textView
+    }()
+    
+    let name: UILabel = {
+        let label = UILabel()
+        label.text = "Eugene"
+        label.font = UIFont(name: "Helvetica", size: 14)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = UIColor.lightGray
+        return label
     }()
     
     fileprivate func setupViews() {
         addSubview(messageTextView)
+        addSubview(name)
         updateConstraints()
     }
     
     override func updateConstraints() {
         super.updateConstraints()
-        messageTextView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 8).isActive = true
+        name.topAnchor.constraint(equalTo: topAnchor, constant: 8).isActive = true
+        name.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 8).isActive = true
+        name.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
+        
+        messageTextView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16).isActive = true
         messageTextView.trailingAnchor.constraint(lessThanOrEqualTo: trailingAnchor, constant: -8).isActive = true
-        messageTextView.topAnchor.constraint(equalTo: topAnchor, constant: 8).isActive = true
+        messageTextView.topAnchor.constraint(equalTo: name.bottomAnchor, constant: 8).isActive = true
         messageTextView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -8).isActive = true
     }
     
